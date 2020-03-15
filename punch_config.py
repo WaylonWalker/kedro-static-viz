c4e4acf42d35d4b2295ad56f8962ec0e0eba0524
Waylon Walker <quadmx08@gmail.com>
2020-03-15 15:28:09 -0500

    enabled commit message on punch

--- punch_config.py
+++ punch_config.py
@@ -8,9 +8,9 @@ FILES = ["setup.py", "kedro_static_viz/__init__.py", "kedro_static_viz/cli.py"]
 
 VERSION = ["major", "minor", "patch"]
 
-# VCS = {
-#     'name': 'git',
-#     'commit_message': (
-#         "Version updated from {{ current_version }}"
-#         " to {{ new_version }}")
-# }
+VCS = {
+    'name': 'git',
+    'commit_message': (
+        "Version updated from {{ current_version }}"
+        " to {{ new_version }}")
+}
