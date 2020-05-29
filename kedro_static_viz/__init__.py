3ff23320856b9633c518be8a695e043f7a70036f
Waylon Walker <quadmx08@gmail.com>
2020-05-28 22:23:46 -0500

    lifted main features to the top of the api

--- kedro_static_viz/__init__.py
+++ kedro_static_viz/__init__.py
@@ -1,2 +1,8 @@
 "kedro-static-viz"
 __version__ = "0.2.3"
+
+__all__ = ["cli", "static_viz", "static_viz_hook"]
+
+from .cli import cli
+from .core import static_viz
+from .hooks import static_viz_hook
